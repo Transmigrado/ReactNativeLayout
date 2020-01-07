3fbed366b97015787397c72f2495254b7f2da6b0
transmigrado <transmigrado@gmail.com>
2020-01-07 12:51:08 -0300

    add Row

---
+++ src/components/Row.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const Row: () => React$Node = () => {
+    return (
+      <>
+        <View>
+            <Text>Hello world</Text>
+        </View>
+      </>
+    );
+  };
+  
+  export default Row
